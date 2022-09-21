22c3cbb35a37a3e313eaba8851d75187807e5651
Rachel Wallis <rachelwalliswork@gmail.com>
2022-09-21 09:22:57 +0100

    trying yet again to fix config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,20 @@ module.exports = {
     },
     {
       resolve: "gatsby-plugin-manifest",
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
+        options: {
+          name: "GatsbyJS",
+          short_name: "GatsbyJS",
+          start_url: "/",
+          background_color: "#6b37bf",
+          theme_color: "#6b37bf",
+          // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+          // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+          display: "standalone",
+          icon: "src/images/icon.png", // This path is relative to the root of the site.
+          // An optional attribute which provides support for CORS check.
+          // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+          // Any invalid keyword or empty string defaults to `anonymous`
+          crossOrigin: `use-credentials`,
       }
     },
   ],
