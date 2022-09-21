95eae5e529eb370fb64740422a8be64ce649503b
Rachel Wallis <rachelwalliswork@gmail.com>
2022-09-21 09:40:49 +0100

    revert config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,17 +20,5 @@ module.exports = {
         host: process.env.CONTENTFUL_HOST
       },
     },
-    {
-      resolve: "gatsby-plugin-manifest",
-        options: {
-          name: "GatsbyJS",
-          short_name: "GatsbyJS",
-          start_url: "/",
-          background_color: "#6b37bf",
-          theme_color: "#6b37bf",
-          display: "standalone",
-          icon: "src/images/icon.png", 
-      }
-    },
   ],
 };
